076a7566af1e2e2c5a7985fd4433dfdf949fa347
Vladimir Medvedev <vladmirworld@gmail.com>
2020-01-15 23:24:04 +0300

    firsf lesson

---
+++ script/degrees.js
@@ -0,0 +1,3 @@
+var celsiusDegrees = prompt('Введите темепературу в градусах по цельсию');
+var fahrenheitDegrees = (9/5)*celsiusDegrees+32;
+alert('Температура по Фарингейту равана ' + fahrenheitDegrees);
